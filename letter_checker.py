4671a185429406fefce3122c0edb25349eb3c922
Kim Richard Joungyoon <richard@Richards-MacBook-Pro.local>
2021-04-16 20:17:58 +0900

    More updates~

--- letter_checker.py
+++ letter_checker.py
@@ -36,18 +36,30 @@ def letter_checker3(string):
         		maxchr = string[i]
 
 	return (maxchr,maxcount)
-
+ 
 print (letter_checker3("iiiiff"))
 
 def letter_checker4(string):
-	maxcount = 0
-	maxchar = " "
-	for i in range (len(string)):
-		if string.count(string[i]) > maxcount:
-			maxcount = string.count(string[i]) 
-			maxchar = string[i]
+	string = string.lower()			#Make all of the characters lowercase
+	maxcount = 0					
+	maxchar = ""
+	sent = ""
+	string = string.replace(" ","")		#Remove all the spaces in the sentence
+	while string != "":					#Loop the for loop until we have no more characters
+		for i in range (len(string)):	#Loop through each character in the string 
+			if string[i].isalpha():		#Make sure the character chosen is an alphabet and not a blank
+				if string.count(string[i]) > maxcount:		#Check which character is most common
+					maxcount = string.count(string[i]) 
+					maxchar = string[i]
+				elif string.count(string[i]) == maxcount:	#Put in alphabetical order if there is the same amount of one character
+					if string[i]<maxchar:
+						maxchar = string[i]
 
-	return (maxchar,maxcount)
+		sent += maxchar		#Append the current most frequent character to the sentence
+		string = string.replace (maxchar, "")	#Replace the current most frequent character with a blank ; basicaly remove the current most frequent letter to make room for the next one
+		maxcount = 0		#Reset the max count so that we can start checking for the next most freqeuent character
+
+	return (sent)
 
 
-print (letter_checker4("apple"))
+print (letter_checker4("I am a boy"))
